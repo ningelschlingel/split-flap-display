369d6f632d9b7b97ca51d3795a231fc7adc00d44
Jan Ningelgen <jan.ningelgen96@googlemail.com>
2026-02-05 18:44:42 +0100

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Controller } from './controller/controller';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Controller],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
